0acd5df412019129228fb5f4ac3a1a779bdfbd78
Sung Min Park <vamf12@gmail.com>
2013-04-23 00:47:36 +0900

    add simple redirect

--- rwm/wiki.py
+++ rwm/wiki.py
@@ -1,3 +1,4 @@
+from urllib import urlopen
 from flaskex import Blueprint
 
 wiki = Blueprint('wiki', __name__)
@@ -6,3 +7,11 @@ wiki = Blueprint('wiki', __name__)
 @wiki.route('/')
 def index():
     return 'Rigveda Wiki Mirror'
+
+
+@wiki.route('/wiki/<path:document>')
+def page(document):
+    return \
+        urlopen(
+            "http://www.rigvedawiki.net/r1/wiki.php/%s?action=raw" % document
+        ).read()
